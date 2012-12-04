fd811da5414d933a06f47979d8ff0738d566b41e
Marcin Kurtz <kurtz,marcin@gmail.com>
2012-12-04 21:49:39 +0100

    C01 Start
    Pierwszy commit projetu. Dodanie pierwszych plików.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+Projekt z programowania w C++
+Programiści: Noemi Szypuła, Koarol Piotrowicz, Marcin Kurtz
+
+Opis projektu w OPISPROJEKTU.txt
+
+Opis pliku: główny plik programu.
+Data utworzenia:  4.12.2012
+Wersja: 0.01
+/**/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Projekt NKMProjekt" << endl;
+	return 0;
+}
