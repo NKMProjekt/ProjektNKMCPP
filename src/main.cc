dd4c10e863d2ea2318b08a8e815b9e2d7bf75925
NKMProjekt <nkmprojekt@gmail.com>
2012-12-16 19:08:35 +0100

    C09 Stowrzenie pierwszych wążnych klass programu.

--- src/main.cc
+++ src/main.cc
@@ -7,69 +7,16 @@
 
 #include <gtkmm.h>
 #include <iostream>
+#include "tetris.h"
 #include "config.h"
-
-/* For testing propose use the local (not installed) ui file */
-/* #define UI_FILE PACKAGE_DATA_DIR"/ui/tetris.ui" */
-#define UI_FILE "src/tetris.ui"
-
-Gtk::Dialog* pDialog = 0;
-
-void on_button_clicked()
-{
-	if(pDialog)
-		pDialog -> show();
-}
-
-void on_buttonQuit_clicked()
-{
-	if(pDialog)
-		pDialog -> hide();
-}
 
 int
 main (int argc, char *argv[])
 {
-	Gtk::Main kit(argc, argv);
+	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.tetris");//Referencja do aplikacji GTKMM Tetris++
 
+	Tetris window;
 
-	//Ładowanie UI z pliku glade
-	Glib::RefPtr<Gtk::Builder> builder;
-
-	try
-	{
-		builder = Gtk::Builder::create_from_file(UI_FILE);
-	}
-	catch (const Glib::FileError & ex)
-	{
-		std::cerr << ex.what() << std::endl;
-		return 1;
-	}
-
-	
-	Gtk::Window* main_win = 0;
-	builder->get_widget("main_window", main_win);
-
-	builder->get_widget("dialogTest",pDialog);
-
-	if (main_win)
-	{
-		Gtk::Button* button = 0;
-		builder->get_widget("button_test", button);
-
-		Gtk::Button* buttonQuit = 0;
-		builder->get_widget("button_quit", buttonQuit);
-
-		if(button && buttonQuit)
-		{
-			button->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-			buttonQuit->signal_clicked ().connect(sigc::ptr_fun(on_buttonQuit_clicked));
-		}
-		
-		kit.run(*main_win);
-	}
-
-	delete(main_win);
-	return 0;
+	return app->run(window);
 }
 
